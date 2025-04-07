2e55fdbac1968a7384ea89be8363e04dad04b706
Matthew Eddey <matt.eddey@gmail.com>
2025-04-07 16:05:03 -0700

    Get build working

--- cmd/run.go
+++ cmd/run.go
@@ -23,23 +23,13 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(_ *cobra.Command, args []string) error {
 		return cobraFxAdapter(fx.Invoke(func(ctx context.Context, s stage.Stage, logger *slog.Logger) {
-			logger.InfoContext(ctx, "Hello, world", "stage", s.String())
+			logger.InfoContext(ctx, "Hello, world", "stage", s.String(), "args", args)
 		}))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
